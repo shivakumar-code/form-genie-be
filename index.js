f9282f504261a4789dd69bd27d59686426e1f655
Shivakumar-code <gshivakumar236@gmail.com>
2025-07-25 04:30:27 +0530

    Added port

--- index.js
+++ index.js
@@ -17,5 +17,5 @@ app.options('*', cors(corsOptions));
 
 app.use(bodyParser.json());
 app.use('/api/auth', authRoutes);
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`✅ Server running on http://localhost:${PORT}`));
