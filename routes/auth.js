d4be1c5e10ad090353ffe1cde0745e11273999d8
SassA <Sahitya.911@gmail.com>
2025-07-24 19:51:39 +0530

    feat: facescan be changes

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
 const { getUserById } = require('../controllers/userController');
 const { sendOtpHandler, verifyOtpAndFetchData } = require('../controllers/authController');
 
-router.post('/send-otp', sendOtpHandler);
+const storage = multer.diskStorage({
+  destination: './uploads',
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + file.originalname);
+  }
+});
+const upload = multer({ storage });
+
+// router.get('/app/:id', getUserById);
+router.post('/send-otp',upload.single('file'), sendOtpHandler);
 router.post('/verify-otp-fetch-data', verifyOtpAndFetchData);
 
 module.exports = router;
