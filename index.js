948f3a7b9ed43fee254e25b586bb14303fe39ec0
Shivakumar-code <gshivakumar236@gmail.com>
2025-07-25 04:06:41 +0530

    Added Changes for Cors

--- index.js
+++ index.js
@@ -4,17 +4,18 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const authRoutes = require('./routes/auth');
-const app = express();  
+const app = express();
 
-app.use(cors({
-  origin: 'https://form-genie-fe-475414324273.europe-west1.run.app', // frontend URL
+const corsOptions = {
+  origin: 'https://form-genie-fe-475414324273.europe-west1.run.app',
   methods: ['GET', 'POST', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
 
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 
 app.use(bodyParser.json());
-
 app.use('/api/auth', authRoutes);
 const PORT = 5000;
 app.listen(PORT, () => console.log(`✅ Server running on http://localhost:${PORT}`));
