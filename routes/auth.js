ee7cf3eaef142682b9bad19953e6a353dbf451c5
Vyshnavi <kvyshnavi.98@gmail.com>
2025-07-24 21:30:05 +0530

    Code refactoring

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const { getUserById } = require('../controllers/userController');
 const { sendOtpHandler, verifyOtpAndFetchData } = require('../controllers/authController');
 
 const storage = multer.diskStorage({
@@ -12,8 +11,7 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
-// router.get('/app/:id', getUserById);
 router.post('/send-otp',upload.single('file'), sendOtpHandler);
-router.post('/verify-otp-fetch-data', verifyOtpAndFetchData);
+router.post('/verify-otp', verifyOtpAndFetchData);
 
 module.exports = router;
