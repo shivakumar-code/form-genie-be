b39f534584242352ff7d54e45a3285687cd65eb4
Vyshnavi-Kottamasu <kvyshnavi.98@gmail.com>
2025-08-01 16:14:13 +0530

    formatted the code

--- routes/auth.js
+++ routes/auth.js
@@ -1,20 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const multer = require('multer');
-const cors = require('cors');
+const multer = require("multer");
+const cors = require("cors");
 
-const { sendOtpHandler, verifyOtpAndFetchData } = require('../controllers/authController');
-
+const {
+  sendOtpHandler,
+  verifyOtpAndFetchData,
+} = require("../controllers/authController");
 
 const storage = multer.diskStorage({
-  destination: './uploads',
+  destination: "./uploads",
   filename: (req, file, cb) => {
     cb(null, Date.now() + file.originalname);
-  }
+  },
 });
 const upload = multer({ storage });
-router.post('/send-otp',  upload.single('file'), sendOtpHandler);
-
-router.post('/verify-otp', verifyOtpAndFetchData);
+router.post("/send-otp", upload.single("file"), sendOtpHandler);
+router.post("/verify-otp", verifyOtpAndFetchData);
 
 module.exports = router;
